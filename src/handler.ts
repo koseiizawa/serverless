bab39d77b29686c621b98d06f701db2f7933f135
koseiizawa <koseiizawa@gmail.com>
2026-04-22 06:13:49 -0700

    add sendData

--- src/handler.ts
+++ src/handler.ts
@@ -6,3 +6,14 @@ export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProx
     body: JSON.stringify({ message: 'Hello World 2026!' }),
   };
 };
+
+export const sendData = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const envVars = process.env;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      environment: envVars,
+    })
+  };
+};
