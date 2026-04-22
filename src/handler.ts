bee51919ea1f262a24d851ecf50409d886f6dfb4
koseiizawa <koseiizawa@gmail.com>
2026-04-22 05:48:23 -0700

    change module

--- src/handler.ts
+++ src/handler.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World!' }),
+    body: JSON.stringify({ message: 'Hello World 2026!' }),
   };
 };
